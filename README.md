230efb9b19cb4c8cc1f88d7685bcbe54f6fa2f97
Dinesh <official.dinesh.profile@gmail.com>
2024-10-03 03:09:33 -0500

    Latest code push

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ A modular, reconfigurable data platform.
 - Multi-Paradigm Data Storage
 - Dynamic Storage Model Reconfiguration
 ...
+
+gcc -I/mingw64/include -L/mingw64/lib -o storium src/main.c src/core/kvstore.c src/api/rest_api.c src/ui/web_interface.c -lmicrohttpd
+
+gcc -IC:/msys64/mingw64/include -LC:/msys64/mingw64/lib -o storium src/main.c src/core/kvstore.c src/api/rest_api.c src/ui/web_interface.c -lmicrohttpd
+
+gcc -IC:/msys64/mingw64/include -I./src/core -LC:/msys64/mingw64/lib -o storium src/main.c src/core/kvstore.c src/api/rest_api.c src/ui/web_interface.c -lmicrohttpd
+
+gcc -IC:/msys64/mingw64/include -I./src/core -LC:/msys64/mingw64/lib -o storium src/main.c src/core/kvstore.c src/api/rest_api.c src/ui/web_interface.c -lmicrohttpd
+
+gcc -I/mingw64/include -I./src/core -L/mingw64/lib -o storium src/main.c src/core/kvstore.c src/api/rest_api.c src/ui/web_interface.c -lmicrohttpd
--- src/ui/web_interface.c
+++ src/ui/web_interface.c
@@ -1,4 +1,39 @@
-// Web UI interface
-void start_web_ui() {
-    // Start web UI server
+#include <stdio.h>
+#include <microhttpd.h>
+
+enum MHD_Result send_response(struct MHD_Connection *connection, const char *message, int status_code);
+
+const char *html_form = "<html><body>"
+                        "<form action='/set' method='POST'>"
+                        "Key: <input type='text' name='key'><br>"
+                        "Value: <input type='text' name='value'><br>"
+                        "<input type='submit' value='Store'>"
+                        "</form>"
+                        "</body></html>";
+
+// Serve the web UI
+static enum MHD_Result web_request_handler(void *cls, struct MHD_Connection *connection, const char *url,
+                                           const char *method, const char *version, const char *upload_data,
+                                           size_t *upload_data_size, void **con_cls)
+{
+    return send_response(connection, html_form, MHD_HTTP_OK);
+}
+
+// Start the web UI
+void start_web_ui()
+{
+    struct MHD_Daemon *daemon;
+
+    daemon = MHD_start_daemon(MHD_USE_SELECT_INTERNALLY, 8080, NULL, NULL, &web_request_handler, NULL, MHD_OPTION_END);
+    if (NULL == daemon)
+    {
+        fprintf(stderr, "Error starting Web UI.\n");
+        return;
+    }
+
+    printf("Web UI running on port 8080...\n");
+
+    getchar();
+
+    MHD_stop_daemon(daemon);
 }
